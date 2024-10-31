a3ad93cf3587acdc602f9f3a43ddabd6c6658785
Graham Arnett <GrahamKentArnett@outlook.com>
2024-10-30 20:09:24 -0400

    init commit

---
+++ ToDoListApp/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace ToDoListApp
+
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TO DO LIST");
+
+            List<Task> tasks = new List<Task>();
+            string command = "";
+
+            while (command != "exit")
+            {
+                Console.Write("Please enter a command (add, list, complete, exit): ");
+                command = Console.ReadLine();
+                switch (command.ToLower())
+                {
+                    case "add":
+                        Console.Write("Enter task name: ");
+                        string taskName = Console.ReadLine();
+                        tasks.Add(new Task(taskName));
+                        break;
+                    case "list":
+                        Console.WriteLine();
+                        foreach (var task in tasks)
+                        {
+                            Console.WriteLine(task);
+                        }
+                        break;
+                    case "complete":
+                    Console.Write("Enter the index of the task to complete: ");
+                    if(int.TryParse(Console.ReadLine(), out int index) && index >= 0 && index < tasks.Count)
+                        {
+                            tasks[index].IsCompleted = true;
+                            Console.WriteLine($"\n{tasks[index].Name} completed");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid index.");
+                        }
+                        break;
+                    case "exit":
+                        Console.WriteLine("\n\nThank you for using the To Do app");
+                        break;
+
+                    default:
+                        Console.Write("\nUnknown command. Please enter 'add', 'list', 'complete', or 'exit'");
+                        break;
+
+                }
+            }
+        }
+    }
+}
---
+++ ToDoListApp/Task.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoListApp
+{
+    public class Task
+    {
+        public string Name {  get; set; }
+        public bool IsCompleted { get; set; }
+
+        public Task(String name) 
+        {
+        Name = name;
+        IsCompleted = false;
+        }
+        public override string ToString() 
+        {
+            return $"{(IsCompleted ? "[X]" : "[ ]")} {Name}";
+        }
+    }
+
+}
